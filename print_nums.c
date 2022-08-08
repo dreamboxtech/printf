6cffae90c8e7e7297c6e920dc9f495f2ae6c9ff4
dreamboxtech <bbbolaleye@gmail.com>
2022-08-08 14:39:54 -0700

    Unsigned Int added

--- print_nums.c
+++ print_nums.c
@@ -70,4 +70,18 @@ void print_number(int n)
 }
 
 
+/**
+ * print_unsigned - prints an unsigned integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_unsigned(va_list l, flags_t *f)
+{
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
+	(void)f;
+	return (_puts(str));
+}
