8300846d67781fc9f24346f805b77a3287857eb3
LoloDum <chinedumokamgba@gmail.com>
2022-08-08 09:48:14 -0700

    print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -32,3 +32,16 @@ int print_char(va_list l, flags_t *f)
 	_putchar(va_arg(l, int));
 	return (1);
 }
+
+/**
+ * print_percent - Prints a percent
+ * @l: va_list arguments from _printf
+ * @f: Pointer to the struct flags in which we turn the flags on
+ * Return: Number of char printed
+ */
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
