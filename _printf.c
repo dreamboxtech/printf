42e461ff0f4eb0922a15a9c167a24a66afdd21c0
dreamboxtech <bbbolaleye@gmail.com>
2022-08-08 09:21:52 -0700

    modified _printf

--- _printf.c
+++ _printf.c
@@ -11,3 +11,14 @@
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
