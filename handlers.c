a3def4f4d27df57d54ec3135cf5f55e7fd69375b
LoloDum <chinedumokamgba@gmail.com>
2022-08-09 17:26:03 -0700

    handlers.c

--- handlers.c
+++ handlers.c
@@ -8,8 +8,7 @@
  * Return: If a conversion function is matched - a pointer to the function.
  * Otherwise - NULL.
  */
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char)
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *, unsigned char, int, int, unsigned char)
 {
 	int i;
 	converter_t converters[] = {
