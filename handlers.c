7649f423488b3c5576c5fa03d7da475043875fae
LoloDum <chinedumokamgba@gmail.com>
2022-08-09 17:16:10 -0700

    handlers.c

--- handlers.c
+++ handlers.c
@@ -38,3 +38,28 @@ unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 
 	return (NULL);
 }
+
+/**
+ * handle_length - Matches length modifiers with their corresponding value.
+ * @modifier: A pointer to a potential length modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a lenth modifier is matched - its corresponding value.
+ * Otherwise - 0.
+ */
+unsigned char handle_length(const char *modifier, char *index)
+{
+	if (*modifier == 'h')
+	{
+		(*index)++;
+		return (SHORT);
+	}
+
+	else if (*modifier == 'l')
+	{
+		(*index)++;
+		return (LONG);
+	}
+
+	return (0);
+}
